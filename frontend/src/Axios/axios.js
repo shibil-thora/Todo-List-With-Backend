02d8bd714880e3d1a66d4a1d24e8ef011704774f
shibil-thora <shibilallakkat123@gmail.com>
2024-07-25 05:11:09 +0530

    added vercel frontend

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios' 
-export const baseURL = 'http://127.0.0.1:8000'
-export const domainPort = '127.0.0.1:8000'
+export const baseURL = 'https://pos-app-stock-management.onrender.com'
+export const domainPort = 'pos-app-stock-management.onrender.com' 
  
 
 const axiosInstance = axios.create({
