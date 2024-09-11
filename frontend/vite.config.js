a1deb3a0ff071c3862e4083b95bd0dab864003a6
shibil-thora <shibilallakkat123@gmail.com>
2024-09-11 20:41:37 +0530

    added manifest file and updated config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Shibil To Do',
+        short_name: 'App',
+        description: 'Your App Description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwas.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwas.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
